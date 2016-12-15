47ae962a89e10b8403684e17f6a6a4449afaf864
Andrei Polzounov <and.pol5@gmail.com>
2016-12-15 20:24:33 +0800

    Cmake working with boost gil and libjpeg/libpng/libtiff

--- src/main.cpp
+++ src/main.cpp
@@ -2,73 +2,29 @@
  *  Image processing micro library
  *  @author Andrei Polzounov
  */
-#include <chrono>
 #include <iostream>
 
-#include <boost/asio.hpp>
-#include <boost/thread.hpp>
+#include <boost/gil/extension/io/jpeg_io.hpp>
+#include <boost/gil/extension/io/png_io.hpp>
+#include <boost/gil/extension/io/tiff_io.hpp>
+#include <boost/gil/gil_all.hpp>
 
-#include "../include/CommsNodeMulticastSender.h"
-#include "CommsNodeAsyncTcpServer.h"
-#include "CommsNodeDelayedTcpServer.h"
-
-using boost::asio::ip;
-using boost::asio::ip::tcp;
-
-void serverThread(const ip::address& multicastAddress)
+int main(int /*argc*/, char** /*argv*/)
 {
-  try
-  {
-    boost::asio::io_service ioService;
-    CommsNodeAsyncTcpServer server(ioService, multicastAddress);
-    ioService.run();
-  }
-  catch (std::exception& e)
-  {
-    std::cerr << e.what() << std::endl;
-  }
-}
+  // Test read jpeg
+  boost::gil::rgb16_image_t image1;
+  boost::gil::jpeg_read_and_convert_image("/home/vauser2/dev/instrumental-cv/images/lena.jpg", image1);
+  std::cout << "lena: " << image1.width() << "x" << image1.height() << std::endl;
 
-void clientThread(const ip::address& multicastAddress)
-{
-  try
-  {
-    boost::asio::io_service ioService;
-    CommsNodeMulticastSender multiCastSender(ioService, multicastAddress);
-    ioService.run();
-  }
-  catch (std::exception& e)
-  {
-    std::cerr << e.what() << std::endl;
-  }
-}
+  boost::gil::rgb16_image_t image3;
+  boost::gil::tiff_read_and_convert_image("/home/vauser2/dev/instrumental-cv/images/square.tiff", image3);
+  auto dims3 = image3.dimensions();
+  std::cout << "square: " << image3.width() << "x" << image3.height() << std::endl;
 
-int main(int argc, char* argv[])
-{
-  if (argc != 2)
-  {
-    std::cerr << "Usage: " << argv[0] << " <multicast_address>" << std::endl;
-    std::cerr << "  For IPv4, try:" << std::endl;
-    std::cerr << "    " << argv[0] << " 239.255.0.1" << std::endl;
-    std::cerr << "  For IPv6, try:" << std::endl;
-    std::cerr << "    " << argv[0] << " ff31::8000:1234" << std::endl;
-    return 1;
-  }
+  boost::gil::rgb16_image_t image2;
+  boost::gil::png_read_and_convert_image("/home/vauser2/dev/instrumental-cv/images/tux.png", image2);
+  auto dims2 = image2.dimensions();
+  std::cout << "tux: " << image2.width() << "x" << image2.height() << std::endl;
 
-  boost::system::error_code ipError;
-  ip::address multicastAddress = ip::address::from_string(argv[1], ipError);
-  if (ipError)
-  {
-    std::cerr << ipError.message() << std::endl;
-    return 1;
-  }
-
-  // Using boost::thread for Windows compatibility as the MinGW compiler
-  // does not yet support std::thread
-  boost::thread sThread(&serverThread, multicastAddress);
-  boost::thread cThread(&clientThread, multicastAddress);
-
-  sThread.join();
-  cThread.join();
   return 0;
 }
